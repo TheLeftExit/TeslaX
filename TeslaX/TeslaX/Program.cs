7719e0cf94ab9e9486403c9a4a20fde2de9b89e9
TheLeftExit <nikitabagno@protonmail.com>
2019-05-13 23:22:18 +0300

    Concept, class placeholders

---
+++ TeslaX/TeslaX/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeslaX
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+
+            Application.Run(new MainForm());
+        }
+    }
+}
