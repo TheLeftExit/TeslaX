7a8bd00f9e327f0eb2c1e05f84294fa91c375161
TheLeftExit <nikitabagno@protonmail.com>
2019-12-02 02:09:46 +0300

    Updated addresses for 2.999

--- TeslaX/Static/Process.cs
+++ TeslaX/Static/Process.cs
@@ -69,17 +69,16 @@ namespace TheLeftExit.TeslaX.Static
         };
 
         // Values in the following functions have been pointer-sniped with Cheat Engine.
-        // I'm not sure yet if they change with EXE updates.
-        // Either way, feel free to use them in your app.
+        // Feel free to use them in your app.
 
         public static Point GetPlayer(this ProcessHandle handle)
         {
             var res = new Point();
 
-            byte[] rawx = handle.GetBytes(0x4C8C48, 4, 0xCC0, 0x188, 0x8);
+            byte[] rawx = handle.GetBytes(0x4EEEF8, 4, 0xCC8, 0x188, 0x8);
             res.X = Convert.ToInt32(BitConverter.ToSingle(rawx, 0));
 
-            byte[] rawy = handle.GetBytes(0x4C8C48, 4, 0xCC0, 0x188, 0xC);
+            byte[] rawy = handle.GetBytes(0x4EEEF8, 4, 0xCC8, 0x188, 0xC);
             res.Y = Convert.ToInt32(BitConverter.ToSingle(rawy, 0));
 
             return res;
@@ -88,20 +87,20 @@ namespace TheLeftExit.TeslaX.Static
         public static short GetBlock(this ProcessHandle handle, int x, int y)
         {
             int lastoffset = 0x04 + x * 0x80 + y * 0x3200;
-            byte[] rawx = handle.GetBytes(0x4C8C48, 2, 0xCC0, 0x130, 0x28, lastoffset);
+            byte[] rawx = handle.GetBytes(0x4EEEF8, 2, 0xCC8, 0x130, 0x28, lastoffset);
             return BitConverter.ToInt16(rawx, 0);
         }
 
         public static short GetBackground(this ProcessHandle handle, int x, int y)
         {
             int lastoffset = 0x06 + x * 0x80 + y * 0x3200;
-            byte[] rawx = handle.GetBytes(0x4C8C48, 2, 0xCC0, 0x130, 0x28, lastoffset);
+            byte[] rawx = handle.GetBytes(0x4EEEF8, 2, 0xCC8, 0x130, 0x28, lastoffset);
             return BitConverter.ToInt16(rawx, 0);
         }
 
         public static bool GetDirection(this ProcessHandle handle)
         {
-            byte[] raw = handle.GetBytes(0x4C8C48, 1, 0xCC0, 0x188, 0x131);
+            byte[] raw = handle.GetBytes(0x4EEEF8, 1, 0xCC8, 0x188, 0x131);
             return raw[0] == 0;
         }
     }
